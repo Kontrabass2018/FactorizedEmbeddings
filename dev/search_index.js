26fff61d381e759b9a078b2d572c64a5a6cf3ea0
Documenter.jl <documenter@juliadocs.github.io>
2024-11-21 22:33:20 +0000

    build based on a8c5858

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Factorized-Embeddings-method","page":"-","title":"Factorized Embeddings method","text":"","category":"section"},{"location":"#Main-methods","page":"-","title":"Main methods","text":"","category":"section"},{"location":"","page":"-","title":"-","text":"fit","category":"page"},{"location":"#FactorizedEmbeddings.fit","page":"-","title":"FactorizedEmbeddings.fit","text":"fit(model, data, params) Fits the model to the data. \n\n\n\n\n\n","category":"function"},{"location":"","page":"-","title":"-","text":"fit_transform","category":"page"}]
+}
