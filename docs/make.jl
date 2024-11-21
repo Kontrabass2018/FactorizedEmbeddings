c7cec1f00920eef43510f597db3096282e8dd261
sauves <leonardsauve@gmail.com>
2024-11-21 17:22:20 -0500

    tried fix

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
 using Documenter, FactorizedEmbeddings
 push!(LOAD_PATH, "../src/")
-makedocs(sitename="Factorized Embeddings Docunentation ")
+makedocs(sitename="Factorized Embeddings Documentation")
 deploydocs(; repo="https://github.com/Kontrabass2018/FactorizedEmbeddings")
