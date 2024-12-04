c743291b5cdc43e8f5f080e84690edb9c23bc540
sauves <leonardsauve@gmail.com>
2024-12-04 13:50:33 -0500

    removed old test folder

--- test_files/testPackage.jl
+++ test_files/testPackage.jl
@@ -14,15 +14,18 @@ folds = split_train_test(TCGA_data);
 
 train_data = folds[1]["train_x"];
 test_data = folds[1]["test_x"];
-model_params = generate_params(train_data, nsteps_dim_redux = 10_000, nsamples_batchsize=1)
+model_params = generate_params(train_data, nsteps_dim_redux = 100_000, nsamples_batchsize=1)
 model = FactorizedEmbeddings.fit(train_data, model_params;verbose = 1);
 # infer
 infer_model, model_phase_1 = FactorizedEmbeddings.infer(model, train_data, test_data, model_params, verbose = 1)
 # plot 
 
 train_embed = cpu(model[1][1].weight)
+test_embed = model_phase_1
 
 train_ids = folds[1]["train_ids"]
+test_ids = folds[1]["test_ids"]
+
 TCGA_colors_file = "TCGA_colors_def.txt"
 fig = Figure(size = (1024,800));
 ax = Axis(fig[1,1],title="Train and Test patient embedding", xlabel = "Patient-FE-1", ylabel="Patient-FE-2", aspect = 1);
@@ -34,5 +37,32 @@ for (i, group_lab) in enumerate(unique(labs))
     name = colors_labels_df[colors_labels_df[:,"labs"] .== group_lab,"name"][1]
     scatter!(ax, train_embed[1,group], train_embed[2,group], strokewidth = 0.1, color = String(col), label = name, marker = :circle)
 end 
+for (i, group_lab) in enumerate(unique(labs))
+    group = labs[test_ids] .== group_lab
+    col = colors_labels_df[colors_labels_df[:,"labs"] .== group_lab,"hexcolor"][1]
+    name = colors_labels_df[colors_labels_df[:,"labs"] .== group_lab,"name"][1]
+    scatter!(ax, test_embed[1,group], test_embed[2,group], strokewidth = 1, color = String(col), label = name, marker = :utriangle)
+end 
 fig
+
+test_embed = cpu(infer_model[1][1].weight);
+fig = Figure(size = (1024,800));
+ax = Axis(fig[1,1],title="Train and Test patient embedding", xlabel = "Patient-FE-1", ylabel="Patient-FE-2", aspect = 1);
+colors_labels_df = CSV.read(TCGA_colors_file,  DataFrame)
+# first plot train embed with circles.
+for (i, group_lab) in enumerate(unique(labs))
+    group = labs[train_ids] .== group_lab
+    col = colors_labels_df[colors_labels_df[:,"labs"] .== group_lab,"hexcolor"][1]
+    name = colors_labels_df[colors_labels_df[:,"labs"] .== group_lab,"name"][1]
+    scatter!(ax, train_embed[1,group], train_embed[2,group], strokewidth = 0.1, color = String(col), label = name, marker = :circle)
+end 
+for (i, group_lab) in enumerate(unique(labs))
+    group = labs[test_ids] .== group_lab
+    col = colors_labels_df[colors_labels_df[:,"labs"] .== group_lab,"hexcolor"][1]
+    name = colors_labels_df[colors_labels_df[:,"labs"] .== group_lab,"name"][1]
+    scatter!(ax, test_embed[1,group], test_embed[2,group], strokewidth = 1, color = String(col), label = name, marker = :utriangle)
+end 
+fig
+
+
 
