36daa77c04c39ac225c28b9386a8a15e9bf105ae
sauves <leonardsauve@gmail.com>
2025-01-14 16:31:33 -0500

    added gallery.md

--- docs/make.jl
+++ docs/make.jl
@@ -2,5 +2,6 @@ using Documenter, FactorizedEmbeddings
 push!(LOAD_PATH, "../src/")
 makedocs(sitename="Factorized Embeddings Documentation",
 pages = ["Home"=>"index.md",
-"Guide" => "guide.md"])
+"Guide" => "guide.md", 
+"Gallery" => "gallery.md"])
 deploydocs(; repo="https://github.com/Kontrabass2018/FactorizedEmbeddings")
---
+++ docs/src/gallery.md
@@ -0,0 +1,5 @@
+# Factorized Embeddings method
+## Gallery
+### 2D visualization of whole TCGA trasncriptomic dataset by cancer type
+
+![TCGA with FE](assets/FE_TCGA_2D.png)
